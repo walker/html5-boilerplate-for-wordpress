6552a9aa88a5a7f94324a3166c9416965f5a1858
walker <myself@walkerhamilton.com>
2011-05-14 09:31:41 -0400

    Local jquery loads in properly now

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 
   <!-- Grab Google CDN's jQuery. fall back to local if necessary, we're doing it here, rather than footer, so that plugins have it available when they almost all bind to wp_head() -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write("<script src='js/libs/jquery-1.6.1.min.js'>\x3C/script>")</script>
+  <script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/libs/jquery-1.6.1.min.js'>\x3C/script>")</script>
 
   <!-- Wordpress Head Items -->
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
