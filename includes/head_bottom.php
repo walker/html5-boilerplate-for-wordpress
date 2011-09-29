bcd09ae1449549545ab9da1c748dfab5b3f02f1f
Walker Hamilton <myself@walkerhamilton.com>
2011-09-29 10:09:03 -0400

    Updated jquery

--- includes/head_bottom.php
+++ includes/head_bottom.php
@@ -13,8 +13,8 @@
 	
 	<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
 	<!-- we're doing it here, rather than footer, so that plugins have it available as they almost all bind to wp_head() -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/libs/jquery-1.6.2.min.js'>\x3C/script>")</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	<script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/libs/jquery-1.6.4.min.js'>\x3C/script>")</script>
 	
 	<!-- Wordpress Head Items -->
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
