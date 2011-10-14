7352eb57efbb391a4fd278563cf8d46712671250
Walker Hamilton <myself@walkerhamilton.com>
2011-10-14 08:45:40 -0400

    Slight changes to bring back up to parity with h5bp project. Still not there on multilingual or child-theme support

--- includes/head_bottom.php
+++ includes/head_bottom.php
@@ -1,12 +1,17 @@
 	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory, if you can: mathiasbynens.be/notes/touch-icons -->
-	<link rel="shortcut icon" href="/favicon.ico">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+	<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
 	
 	<!-- CSS : implied media="all" -->
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	
+	<!-- All JavaScript at the bottom, except this Modernizr build incl. Respond.js
+		Respond is a polyfill for min/max-width media queries. Modernizr enables HTML5 elements & feature detects; 
+		for optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ -->
+	<script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
 	
 	<!-- (Almost) All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
