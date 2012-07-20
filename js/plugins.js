571f4723209780103c5e67b271a33a15711c11d5
Walker Hamilton <myself@walkerhamilton.com>
2012-07-20 14:24:12 -0500

    Added in new console suppressor

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,5 @@
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-
 window.log = function f() {
     log.history = log.history || [];
     log.history.push(arguments);
@@ -24,22 +23,17 @@ window.log = function f() {
     }
 };
 
-// make it safe to use console.log always
-
-(function(a) {
-    function b() {}
-    var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn";
-    var d;
-    for (c = c.split(","); !!(d = c.pop());) {
-        a[d] = a[d] || b;
-    }
-})(function() {
-    try {
-        console.log();
-        return window.console;
-    } catch(a) {
-        return (window.console = {});
-    }
-}());
+// Avoid `console` errors in browsers that lack a console
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
