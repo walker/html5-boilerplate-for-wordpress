f02ad60d4b64c0e16bf4fc0cef47b0f322aaf64f
Walker Hamilton <myself@walkerhamilton.com>
2012-07-20 14:20:00 -0500

    Updated Modernizr to 2.6.1

--- includes/head_bottom.php
+++ includes/head_bottom.php
@@ -10,7 +10,7 @@
 	<!-- All JavaScript at the bottom, except this Modernizr build incl. Respond.js
 		Respond is a polyfill for min/max-width media queries. Modernizr enables HTML5 elements & feature detects; 
 		for optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ -->
-	<script src="<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.5.3.min.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.6.1.min.js"></script>
 	
 	<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
 	<!-- We're doing it here, rather than footer, so that plugins have it available as they almost all bind to wp_head() -->
