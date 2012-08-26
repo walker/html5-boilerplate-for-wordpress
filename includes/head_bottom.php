81c1b3cb716f824ff83afa48a608d7b92873b71e
Walker Hamilton <myself@walkerhamilton.com>
2012-08-26 12:34:21 -0500

    Changing some items to work a little better...CDN links will be protocol-less with next version of WP: http://core.trac.wordpress.org/ticket/16560

--- includes/head_bottom.php
+++ includes/head_bottom.php
@@ -1,17 +1,16 @@
-	<meta name="viewport" content="width=device-width">
 
-	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
-	<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="<?php echo versioned_resource('style.css'); ?>">
+        <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+        <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
 
-	<!-- Wordpress Head Items -->
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<?php wp_head(); ?>
-    <script>typeof Modernizr == 'object' || document.write("<script src='<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.6.1.min.js'>\x3C/script>")</script>
-    <script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.8.0.min.js'>\x3C/script>")</script>
-</head>
-<body <?php body_class(); ?>>
+        <!-- Wordpress Head Items -->
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+        <?php wp_head(); ?>
+        <script>typeof Modernizr == 'object' || document.write("<script src='<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.6.1.min.js'>\x3C/script>")</script>
+        <script>window.jQuery || document.write("<script src='<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.8.0.min.js'>\x3C/script>")</script>
+    </head>
+    <body <?php body_class(); ?>>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
--- includes/head_top.php
+++ includes/head_top.php
@@ -6,3 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
