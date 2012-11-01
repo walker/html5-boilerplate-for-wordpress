d60fb9adf41e509d74f2e79857e72702bd2eb4bd
Walker Hamilton <myself@walkerhamilton.com>
2012-11-01 09:47:53 -0500

    Remove shim and make sure styles are up to date.

--- includes/head_top.php
+++ includes/head_top.php
@@ -6,7 +6,3 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-        <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
